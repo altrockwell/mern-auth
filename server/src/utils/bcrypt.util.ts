e503f25703662c65f4c61ee6524cd078333d8a03
Jonathan Aplacador <aplacadorjonathan2@gmail.com>
2022-08-05 14:45:39 +0800

    bcrypt password validator

--- server/src/utils/bcrypt.util.ts
+++ server/src/utils/bcrypt.util.ts
@@ -1,10 +1,14 @@
 import bycrpt from 'bcrypt';
 
-export const encryptPassword = async (password: string) => {
-	// should generate salt
-	const salt = await bycrpt.genSalt(10);
+export const encryptPassword = async (password: string): Promise<string> => {
+	try {
+		const salt = await bycrpt.genSalt(10);
+		return await bycrpt.hash(password, salt);
+	} catch (error) {
+		throw new Error('Error in generating salt');
+	}
+};
 
-	// should hash password
-	return await bycrpt.hash(password, salt);
-	// should not return plain password
+export const isPasswordValid = async (password: string, hashedPassword: string): Promise<boolean> => {
+	return await bycrpt.compare(password, hashedPassword);
 };
